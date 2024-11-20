389867e3d524e8f0c1880315f3ff1b0d04269f3d
Termi25 <mariusrusu208@gmail.com>
2024-11-20 14:27:53 +0200

    Updated SongPlayer beahaviour for the end of the song and stop button

--- app/src/main/java/com/ase/aplicatiemuzicamobila/MainActivity.java
+++ app/src/main/java/com/ase/aplicatiemuzicamobila/MainActivity.java
@@ -19,17 +19,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.annotation.GlideExtension;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    PlayButton btnPlayPause;
+    SongPlayer btnPlayPause;
     TextView tvSongName;
-    TextView tvCurrentTime;
-    TextView tvFinalTime;
-    SeekBar seekBar;
     TextView load;
     ImageView imgV;
     ImageView imgVAnimated;
@@ -37,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private final ActivityResultLauncher<String> launcher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
         @Override
         public void onActivityResult(Uri o) {
-            btnPlayPause.pauseSong(getApplicationContext());
-            btnPlayPause.setPlaying(false);
+            btnPlayPause.pauseSong();
             btnPlayPause.changeMediaPlayer(MediaPlayer.create(getApplicationContext(),o));
+            btnPlayPause.updateTotalDuration();
 
             MediaMetadataRetriever mediaMetadataRetriever = (MediaMetadataRetriever) new MediaMetadataRetriever();
             mediaMetadataRetriever.setDataSource(getApplicationContext(), o);
@@ -94,25 +88,16 @@ public class MainActivity extends AppCompatActivity {
         imgVAnimated.setImageDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.vynil,null));
 
         tvSongName = findViewById(R.id.tvSongName);
-        tvCurrentTime=findViewById(R.id.tvDurationCurrent);
-        tvFinalTime=findViewById(R.id.tvDurationFinal);
         load = findViewById(R.id.btnLoad);
         load.setOnClickListener(v -> launcher.launch("audio/*"));
 
-        btnPlayPause = PlayButton.getPlayPauseInstance(findViewById(R.id.btnPlay),imgVAnimated,MediaPlayer.create(this,R.raw.bna),getApplicationContext());
-    }
-
-    public void stopSong(View v) {
-        btnPlayPause.stopSong();
-        Glide.with(getApplicationContext())
-                .load(R.drawable.missing)
-                .into(imgV);
+        btnPlayPause = SongPlayer.getPlayPauseInstance(findViewById(R.id.btnPlay),imgVAnimated,MediaPlayer.create(this,R.raw.bna),this);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        PlayButton.releaseMediaPlayer();
+        SongPlayer.releaseMediaPlayer();
     }
 
     public String getFileName(Uri uri) {
@@ -136,49 +121,6 @@ public class MainActivity extends AppCompatActivity {
         }
         result=result.split("\\.")[0];
         return result;
-    }
-
-    public  String milliSecondsToTimer(long milliseconds) {
-        String finalTimerString = "";
-        String secondsString = "";
-
-        // Convert total duration into time
-        int hours = (int) (milliseconds / (1000 * 60 * 60));
-        int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
-        int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
-        // Add hours if there
-        if (hours > 0) {
-            finalTimerString = hours + ":";
-        }
-
-        // Prepending 0 to seconds if it is one digit
-        if (seconds < 10) {
-            secondsString = "0" + seconds;
-        } else {
-            secondsString = "" + seconds;
-        }
-
-        finalTimerString = finalTimerString + minutes + ":" + secondsString;
-
-        // return timer string
-        return finalTimerString;
-    }
-
-    private Runnable mUpdateTime = new Runnable() {
-        public void run() {
-            int currentDuration;
-            if (btnPlayPause.isPlaying()) {
-                currentDuration = btnPlayPause.getMediaPlayer().getCurrentPosition();
-                updatePlayer(currentDuration);
-                tvCurrentTime.postDelayed(this, 1000);
-            }else {
-                tvCurrentTime.removeCallbacks(this);
-            }
-        }
-    };
-
-    private void updatePlayer(int currentDuration){
-        tvCurrentTime.setText("" + milliSecondsToTimer((long) currentDuration));
     }
 }
 
---
+++ app/src/main/java/com/ase/aplicatiemuzicamobila/SongPlayer.java
@@ -0,0 +1,195 @@
+package com.ase.aplicatiemuzicamobila;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import com.bumptech.glide.Glide;
+
+public class SongPlayer {
+    private static SongPlayer btnInstance=null;
+    private ImageButton btnPlayPause;
+    private ImageButton btnStop;
+    private ImageView imgVPlaying;
+    private ImageView imgV;
+    private MediaPlayer mediaPlayer;
+    private Activity activity;
+
+    TextView tvSongName;
+    TextView tvCurrentTime;
+    TextView tvFinalTime;
+    SeekBar seekBar;
+
+    private SongPlayer() {
+    }
+
+    private SongPlayer(ImageButton btn, ImageView imgVVynil, MediaPlayer media, Activity activity){
+        this.btnPlayPause=btn;
+        this.imgVPlaying=imgVVynil;
+        this.activity=activity;
+        this.tvSongName=activity.findViewById(R.id.tvSongName);
+        this.mediaPlayer=media;
+
+        this.mediaPlayer.setOnCompletionListener(mp -> {
+            pauseSong();
+        });
+
+        imgV=activity.findViewById(R.id.imageView);
+        tvCurrentTime=activity.findViewById(R.id.tvDurationCurrent);
+
+        tvFinalTime=activity.findViewById(R.id.tvDurationFinal);
+        tvFinalTime.setText(milliSecondsToTimer((long) this.mediaPlayer.getDuration()));
+
+        seekBar=activity.findViewById(R.id.seekBar);
+        seekBar.setMax(this.mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(mediaPlayer != null && fromUser){
+                    mediaPlayer.seekTo(progress);
+                    updatePlayer(mediaPlayer.getCurrentPosition());
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        this.btnStop=activity.findViewById(R.id.btnStop);
+        this.btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopSong();
+            }
+        });
+
+        this.btnPlayPause.setOnClickListener(v -> {
+            Animation animation= AnimationUtils.loadAnimation(activity.getApplicationContext(),R.anim.vynil_animation);
+            animation.setInterpolator(new LinearInterpolator());
+            animation.setRepeatCount(Animation.INFINITE);
+            animation.setDuration(1400);
+
+            if(mediaPlayer.isPlaying()){
+                pauseSong();
+            }else{
+                playSong(animation);
+            }
+        });
+    }
+
+    public static SongPlayer getPlayPauseInstance(ImageButton btn, ImageView imgVVynil, MediaPlayer media, Activity activity) {
+        if(btnInstance==null){
+            btnInstance=new SongPlayer(btn,imgVVynil,media,activity);
+        }
+        return btnInstance;
+    }
+
+    public static void releaseMediaPlayer(){
+        btnInstance.mediaPlayer.release();
+    }
+
+    public void playSong(Animation animation){
+        btnInstance.mediaPlayer.start();
+        this.imgVPlaying.startAnimation(animation);
+        seekBar.post(mUpdateSeek);
+        btnPlayPause.setBackground(ResourcesCompat.getDrawable(activity.getResources(),R.drawable.pause,null));
+    }
+
+    public void pauseSong() {
+        btnInstance.mediaPlayer.pause();
+        this.imgVPlaying.clearAnimation();
+        this.imgVPlaying.setImageDrawable(ResourcesCompat.getDrawable(activity.getApplicationContext().getResources(),R.drawable.vynil,null));
+        btnPlayPause.setBackground(ResourcesCompat.getDrawable(activity.getResources(),R.drawable.play,null));
+    }
+
+    public void stopSong() {
+        pauseSong();
+        btnInstance.mediaPlayer.stop();
+        tvSongName.setText("Load another song");
+        Glide.with(activity.getApplicationContext())
+                .load(R.drawable.missing)
+                .into(imgV);
+        updateTotalDuration();
+    }
+
+    public void changeMediaPlayer(MediaPlayer media){
+        SongPlayer.releaseMediaPlayer();
+        this.mediaPlayer=media;
+        this.mediaPlayer.setOnCompletionListener(mp -> {
+            pauseSong();
+        });
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return btnInstance.mediaPlayer;
+    }
+
+    public  String milliSecondsToTimer(long milliseconds) {
+        String finalTimerString = "";
+        String secondsString = "";
+
+        // Convert total duration into time
+        int hours = (int) (milliseconds / (1000 * 60 * 60));
+        int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
+        int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+        // Add hours if there
+        if (hours > 0) {
+            finalTimerString = hours + ":";
+        }
+
+        // Prepending 0 to seconds if it is one digit
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = "" + seconds;
+        }
+
+        finalTimerString = finalTimerString + minutes + ":" + secondsString;
+
+        // return timer string
+        return finalTimerString;
+    }
+
+    private Runnable mUpdateSeek = new Runnable() {
+        public void run() {
+            int currentDuration;
+            if (btnInstance.mediaPlayer.isPlaying()) {
+                currentDuration = btnInstance.mediaPlayer.getCurrentPosition();
+                updatePlayer(currentDuration);
+                seekBar.setProgress(currentDuration);
+                seekBar.postDelayed(this,0);
+            }else {
+                seekBar.removeCallbacks(this);
+            }
+        }
+    };
+
+    private void updatePlayer(int currentDuration){
+        tvCurrentTime.setText("" + milliSecondsToTimer((long) currentDuration));
+    }
+
+    public void updateTotalDuration(){
+        tvFinalTime.setText(milliSecondsToTimer((long) this.mediaPlayer.getDuration()));
+        seekBar.setProgress(0);
+        updatePlayer(0);
+        seekBar.setMax(this.mediaPlayer.getDuration());
+    }
+}
